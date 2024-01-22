83e6ea1e8918b39d23afd3b34a4f8bf76c2eca5f
Gonzalo98k <gonzalo98k@gmail.com>
2024-01-22 10:23:40 -0300

    hago un for para recorrer un array de numeros en js.js

--- js.js
+++ js.js
@@ -1,1 +1,7 @@
 console.log("Hola  Mundo!!")
+
+let arr = [1,2,3,4,5]
+
+for (let i = 0; i < arr.length; i++) {
+    arr[i]
+}
--- js2.js
+++ js2.js
@@ -1,1 +1,10 @@
 console.log("Hola  Mundo2!")
+
+const personas = {
+    persona: "Gonzalo Alvarez",
+    saludo: "que onda",
+}
+
+const saludar = ()=>{
+    console.log(personas.saludo)
+}
